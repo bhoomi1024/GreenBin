b601e529a7e165229811dc9986070c99073fe2ba
Bhoomi verma <vbhoomi1024@gmail.com>
2024-10-30 16:07:22 +0530

    Change Login name

--- frontend/src/pages/home/RestaurantLoginRegister.jsx
+++ frontend/src/pages/home/RestaurantLoginRegister.jsx
@@ -107,7 +107,7 @@ const RestaurantLoginRegister = () => {
                 <input
                   type="text"
                   id="restaurantName"
-                  placeholder="Restaurant Name"
+                  placeholder="EcoArtisans Name"
                   className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring-blue-500"
                   value={restaurantName}
                   onChange={(e) => setRestaurantName(e.target.value)}
